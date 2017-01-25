5367dcc95732eb6364208289655c5d64aa87d8fd
Brian Johnston <b_johnston98@yahoo.com>
2017-01-25 18:31:31 -0500

    Ex1 puts to scrn

---
+++ EX1.rb
@@ -0,0 +1,9 @@
+#lrthw - ex1
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd mucn rather you 'not'."
+puts 'I "said" do not touch this.'
